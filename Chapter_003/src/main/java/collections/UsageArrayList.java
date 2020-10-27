9a8d5786b23877bcfa421c4f99c49887328af7e5
leonid3101 <leonid2015@yandex.ru>
2020-10-27 20:44:33 +0300

    Видео [#155173]

--- Chapter_003/src/main/java/collections/UsageArrayList.java
+++ Chapter_003/src/main/java/collections/UsageArrayList.java
@@ -3,7 +3,7 @@ package collections;
 import java.util.ArrayList;
 
 public class UsageArrayList {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
+
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
         names.add("Petr");
@@ -11,7 +11,6 @@ public class UsageArrayList {
         names.add("Stepan");
         for (String value:names) {
             System.out.println(value);
-        }
     }
-
+    }
 }
