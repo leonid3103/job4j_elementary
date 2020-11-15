d32c7ae359e255a95428d07122bbf4267167415b
leonid3101 <leonid2015@yandex.ru>
2020-11-15 23:08:05 +0300

    2. Очередь с приоритетом на LinkedList [#155170]

---
+++ Chapter_003/src/main/java/collections/search/PriorityQueue.java
@@ -0,0 +1,22 @@
+package collections.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+  
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+        if (element.getPriority() > task.getPriority()) {
+            break;
+        }
+        index++;
+        }
+        tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
