9d8ed72fb053c8204a71627099548bf86227c2fc
leonid3101 <leonid2015@yandex.ru>
2020-11-28 14:06:35 +0300

    2. Написать программу преобразования List в Map. [#155177]

---
+++ Chapter_003/src/main/java/collections/OrderConvert.java
@@ -0,0 +1,15 @@
+package collections;
+
+import collections.search.Person;
+
+import java.util.*;
+
+public class OrderConvert {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+        for (Order order : orders) {
+            map.put(order.getNumber(), order);
+        }
+        return map;
+    }
+}
---
+++ Chapter_003/src/test/java/OrderConvertTest.java
@@ -0,0 +1,17 @@
+import collections.Order;
+import collections.OrderConvert;
+import org.junit.Test;
+
+import java.util.*;
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+
+public class OrderConvertTest {
+    @Test
+    public void whenSingleOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+    }
+}
