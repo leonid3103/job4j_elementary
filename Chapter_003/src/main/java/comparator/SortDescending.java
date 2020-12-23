c3afe7e6fd8498e8cabc62555159cf3bb0f3f8fb
leonid3101 <leonid2015@yandex.ru>
2020-12-23 22:48:33 +0300

    0. Видео [#155163]

--- Chapter_003/src/main/java/comparator/SortDescending.java
+++ Chapter_003/src/main/java/comparator/SortDescending.java
@@ -9,15 +9,7 @@ import java.util.List;
 
 public class SortDescending implements Comparator<Item> {
     @Override
-    public int compare(Item o1, Item o2) {
-        int first = Integer.parseInt(o1.getId());
-        int second = Integer.parseInt(o2.getId());
-        if (first == second) {
-            return 0;
-        } else if (first < second) {
-            return 1;
-        } else {
-            return -1;
+    public int compare(Item first, Item second) {
+        return second.getId().compareTo(first.getId());
     }
     }
-}
